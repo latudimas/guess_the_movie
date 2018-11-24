13f0c8bcf4c70fbd2b757067495e666b9a9bb0bb
latudimas <riswandha.ld@gmail.com>
2018-11-24 21:40:13 +0700

    Red Code

--- src/Movies.java
+++ src/Movies.java
@@ -4,92 +4,101 @@ import java.util.Scanner;
 
 public class Movies {
     // Declare instance
-    private String mMovieTitle;
-    private String mTitleHidden;
+    private char[] originalMovieTitle;
+    private char[] maskedMovieTitle;
     private String fileName = "movies_title_file.txt";
-    private ArrayList<String> titleList = new ArrayList<String>();
+//    private ArrayList<String> titleList = new ArrayList<String>();
 
     // Default Constructor
     Movies() {
-//        this.loadData();
+//        this.originalMovieTitle = null;
+//        this.maskedMovieTitle = null;
     }
 
-    // Helper method, transform data from file to array
-    private ArrayList<String> loadData() throws Exception{
-        File file = new File(fileName);
+    /**
+     * This is helper method used to load data in txt format and transform it into array list
+     * Then array list passed into pickRandomTitle method as argument.
+     *
+     * @throws Exception
+     */
+    private ArrayList<String> loadData(String fileNameInput) throws Exception{
+        File file = new File(fileNameInput);
         Scanner scanner = new Scanner(file);
 
+        // Creating array list
+        ArrayList<String> titleList = new ArrayList<String>();
+
         // Add to array list
-        while(scanner.hasNextLine()){
-            String title = scanner.nextLine();
-            titleList.add(title);
+        while(scanner.hasNextLine()) {
+            String titleInLine = scanner.nextLine();
+            titleList.add(titleInLine);
         }
         return titleList;
     }
 
-    // Method to pick random title from array list
-    // Generate random number, and pick one title from list based on index showed on random number
-    public String pickRandomTitle(ArrayList<String> titleListInput){
+    /**
+     * Pick one movie title from list randomly
+     * after that, title which is in String data type transformed into array of char.
+     *
+     * @param titleListInput Array list contain movies title
+     * @return orignialMovieTitle array of char from one movie title that pick randomly
+     */
+    public char[] pickRandomTitle(ArrayList<String> titleListInput){
         int randomNumber = (int) (Math.random()*(24+1));
-        mMovieTitle = titleListInput.get(randomNumber);
-
-        return mMovieTitle;
+        String temp = titleListInput.get(randomNumber);
+        // originalMovieTitle = temp.toCharArray();
+        // return originalMovieTitle;
+        char[] tempChar = temp.toCharArray();
+        return tempChar;
     }
 
-    // Transform title into list of underscore
-    public char[] titleHide (String titleInput) {
-//        String[] tempArray =new String[inputLength];
+    /**
+     * Masked original title char with underscore '_' symbol.
+     * Saved it to objek parameter
+     *
+     * @param titleChar char of original movie title
+     * @return maskedMovieTitle saved it into movie title
+     */
+    public char[] maskingTitle (char[] titleChar) {
+        // Get char length for iteration bound
+        int inputLength = titleChar.length;
 
-        // convert string to array of characters
-        char[] titleChars = titleInput.toCharArray();
-        int inputLength = titleChars.length;
-
-        // checking trough characters array, check if it's letter or not
+        // Loop through title char
         for (int i=0; i<inputLength; i++){
-            if(Character.isLetter(titleChars[i])){
-                titleChars[i] = '_';
+            // Check if this char is letter or not
+            if (Character.isLetter(titleChar[i])){
+                titleChar[i] = '_';
             }
         }
-
-//        for (int i=0; i<inputLength; i++) {
-//
-//
-//            // char c = titleInput.charAt(i); //ambil karakter
-//            // if(Character.isLetter(c)) {} // check tiap karakter
-//            // jika ini huruf, ubah ke underscore symbol
-//            // jika bukan huruf, tetap, jangan diubah
-//
-//        }
-        return titleChars;
+        return titleChar;
     }
 
     public static void main(String [] args){
         Movies myMovies = new Movies();
 
+
+        ArrayList<String> titleList = new ArrayList<>();
+        String fileName = "movies_title_file.txt";
         try{
-            myMovies.loadData();
-            myMovies.pickRandomTitle(myMovies.titleList);
-            System.out.print(myMovies.titleList);
-            System.out.print("\n" + myMovies.mMovieTitle + "\n" + myMovies.mMovieTitle.length());
+            titleList = myMovies.loadData(fileName);
         } catch (Exception e) {
             System.out.print("Data Not Found");
             e.printStackTrace();
         }
 
-        String str = "journal Dev";
-        char[] chars = str.toCharArray();
-        System.out.print("\nstring length: " + str.length());
-        System.out.print("char length: " + chars.length);
+        char[] origin = myMovies.pickRandomTitle(titleList);
+        char[] masked = myMovies.maskingTitle(origin);
 
-        for(int i=0; i<chars.length; i++){
-            if(Character.isLetter(chars[i])){
-                chars[i] = '_';
-            }
-        }
-        System.out.print(chars);
-//        for (char c: chars) {
-//            System.out.print(c + "\n");
-//        }
+        myMovies.originalMovieTitle = origin;
+        myMovies.maskedMovieTitle = masked;
 
+//        System.out.print(myMovies.originalMovieTitle);
+//        System.out.print("\n" + myMovies.maskedMovieTitle);
+        System.out.println(myMovies.pickRandomTitle(titleList) + "\n");
+        System.out.print(titleList + "\n");
+        System.out.print(origin);
+        System.out.print("\n" + masked);
+
+        String hele;
     }
 }
